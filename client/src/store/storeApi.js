a502c4885be0f3160b5ab8573a181e56d76e5045
MuzamilEng <muzekhan777@gmail.com>
2024-09-16 18:10:58 +0500

    updated store api

--- client/src/store/storeApi.js
+++ client/src/store/storeApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const storeApi = createApi({
   reducerPath: "storeApi",
-  baseQuery: fetchBaseQuery({ baseUrl: `https://www.api.funrides.co.uk/api/v1` }),
+  baseQuery: fetchBaseQuery({ baseUrl: `https://www.api.funrides.co.uk` }),
   endpoints: (builder) => ({
     createBooking: builder.mutation({
       query: (data) => ({
