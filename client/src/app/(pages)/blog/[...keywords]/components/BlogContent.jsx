e7435ec7a44186a65951c4fa434c9f17a262412e
afrazrajpoot <afrazrajpoot46@gmail.com>
2025-02-13 21:37:22 +0500

    set the meta image

--- client/src/app/(pages)/blog/[...keywords]/components/BlogContent.jsx
+++ client/src/app/(pages)/blog/[...keywords]/components/BlogContent.jsx
@@ -2,23 +2,13 @@
 import React from 'react';
 import axios from 'axios';
 import Head from 'next/head';
-import dynamic from 'next/dynamic';
-// import Layout from '@/Layout/Layout';
-import Header from '@/components/Header';
 import Layout from '@/Layout/Layout';
+import Header from '@/components/Header';
 import BlogContentClient from './BlogContentClient';
 
-// Dynamically import client components
-// const SocialShare = dynamic(() => import('@/components/SocialShare'), { ssr: false });
-// const BlogContentClient = dynamic(() => import('@/components/BlogContentClient'), { ssr: false });
-
 const BlogContent = async ({ params }) => {
-  const keywords =  params.keywords[0];
+  const keywords = params.keywords[0];
   console.log("Fetching blog with slug:", keywords);
-
-  const encryptionKey = process.env.NEXT_PUBLIC_SECRET_KEY;
-  const adminEmail = 'subadmin@gmail.com';
-  const adminPassword = 'Subadmin@123+';
 
   // Fetch blog data
   const readBlog = async () => {
@@ -26,15 +16,13 @@ const BlogContent = async ({ params }) => {
       const res = await axios.get(
         `${process.env.NEXT_PUBLIC_API_URL}/api/v1/blogs/${keywords}`
       );
-      console.log(res,'ress')
+      console.log("API Response:", res.data);
       return res.data;
-   
     } catch (err) {
       console.error("Error fetching blog:", err);
       return {};
     }
   };
-  
 
   const blogData = await readBlog();
 
@@ -44,6 +32,8 @@ const BlogContent = async ({ params }) => {
   const metaImage = blogData?.image1?.fileName 
     ? `${process.env.NEXT_PUBLIC_API_URL}/${blogData.image1.fileName}`
     : `${process.env.NEXT_PUBLIC_API_URL}/default-image.jpg`; // Fallback image if no blog image
+
+  console.log("Meta Image URL:", metaImage); // Log the meta image URL
 
   // If no data is found, show a message
   if (Object.keys(blogData).length === 0) {
@@ -55,7 +45,7 @@ const BlogContent = async ({ params }) => {
       </Layout>
     );
   }
-console.log(metaImage,'meta image')
+
   return (
     <>
       <Header />
@@ -69,8 +59,8 @@ console.log(metaImage,'meta image')
         <meta property="og:url" content={`https://api.funrides.co.uk/blogs/${keywords}`} />
         <meta property="og:type" content="article" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@Danhamz" />
-        <meta name="twitter:creator" content="@Danhamz" />
+        <meta name="twitter:site" content="@Funfactorial" />
+        <meta name="twitter:creator" content="@Funfactorial" />
         <meta name="twitter:title" content={metaTitle} />
         <meta name="twitter:description" content={metaDescription} />
         <meta name="twitter:image" content={metaImage} />
